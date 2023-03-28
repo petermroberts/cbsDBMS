f7663f48ec81ef60970df194b8f9174ade14a62d
petermroberts <peterroberts7998@gmail.com>
2023-03-28 13:31:47 -0300

    got rid of import F

--- Donors/models.py
+++ Donors/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.db.models import CheckConstraint, Q, F
+from django.db.models import CheckConstraint, Q
 from django.core.exceptions import ValidationError
 from django.forms import ModelForm
 
