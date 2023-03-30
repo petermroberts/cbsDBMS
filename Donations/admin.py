ec4f156631c15996a5a900601513be7053e38277
petermroberts <peterroberts7998@gmail.com>
2023-03-29 22:30:25 -0300

    added admin pages

--- Donations/admin.py
+++ Donations/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+admin.site.register(Donation)
+admin.site.register(BloodDonation)
+admin.site.register(PlasmaDonation)
+admin.site.register(PlateletDonation)
+
+admin.site.register(Sample)
